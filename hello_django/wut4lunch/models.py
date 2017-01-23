1daade23d58a51ef041acc3b328aaae5fae6084d
Name <your email address>
2017-01-23 16:17:34 -0500

    changed dir

--- hello_django/wut4lunch/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Lunch(models.Model):
-    submitter = models.CharField(max_length=63)
-    food = models.CharField(max_length=255)
